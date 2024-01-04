dd0779ec9bb00333a3f8cddeeb13c6e1b8975426
maxca17 <maxcalvert34@gmail.com>
2024-01-03 19:18:17 -0600

    helloooooo

--- sc/src/header.js
+++ sc/src/header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render() {
     return (
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">My Music App</Navbar.Brand>
+        <Navbar.Brand href="#home">My Music App!</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Navbar.Text className="ml-auto text-white">Welcome to the SoundCloud Downloader</Navbar.Text>
